eab7bbdd69a1489cf8602f1c00417aa3037ede4c
drumath2237 <2237k.drumath@gmail.com>
2026-06-27 18:19:53 +0900

    :wrench: setup for wasm32 build

--- math-wasm/src/lib.rs
+++ math-wasm/src/lib.rs
@@ -1,4 +1,5 @@
-pub fn add(left: u64, right: u64) -> u64 {
+#[unsafe(export_name = "add")]
+pub extern "C" fn add(left: f32, right: f32) -> f32 {
     left + right
 }
 
@@ -8,7 +9,7 @@ mod tests {
 
     #[test]
     fn it_works() {
-        let result = add(2, 2);
-        assert_eq!(result, 4);
+        let result = add(2.0, 2.0);
+        assert_eq!(result, 4.0);
     }
 }
