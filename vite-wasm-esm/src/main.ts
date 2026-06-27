6aee0f22d41503b69a25cb085d66b26aa9c72876
drumath2237 <2237k.drumath@gmail.com>
2026-06-27 19:38:12 +0900

    :bug: fix tsc error with type declaration

--- vite-wasm-esm/src/main.ts
+++ vite-wasm-esm/src/main.ts
@@ -1,12 +1,17 @@
 import "./style.css";
 
+import { add } from "../wasm/math_wasm.wasm";
+
 function main() {
   const app = document.getElementById("app");
   if (!(app instanceof HTMLDivElement)) {
     return;
   }
 
-  app.textContent = "Hello!";
+  const result = add(1, 2);
+  console.log(result);
+
+  app.textContent = `add(1, 2) = ${result}`;
 }
 
 main();
